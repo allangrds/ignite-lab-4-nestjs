16db73d92e66217758eedfe50880618084586071
Allan Ramos <allangrds@gmail.com>
2023-02-02 17:17:23 -0300

    feat: add simple get and post notifications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { PrismaService } from './prisma.service';
-import { Controller, Get } from '@nestjs/common';
+import { CreateNotificationBody } from './create-notification-body'
+import { PrismaService } from './prisma.service'
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { randomUUID } from 'node:crypto'
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
-    return this.prisma.notification.findMany();
+  list() {
+    return this.prisma.notification.findMany()
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const resource = await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: body.content,
+        category: body.category,
+        recipientId: body.recipientId,
+      },
+    })
+
+    return {
+      status: 'success',
+      statusCode: 201,
+      id: resource.id,
+      message: 'Notification created',
+    }
   }
 }
